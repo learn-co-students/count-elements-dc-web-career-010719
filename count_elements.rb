a8a8abbd4d6075ebf30398482f5c09247c4331d6
github username <github email address>
2019-01-07 23:00:21 -0500

    finished count element lab 7JAN19

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,18 @@
+require "pry"
+
 def count_elements(array)
-  # code goes here
+  	new_hash = {}
+  	array.each do |a|
+	  	counter = 0
+	  	helper = 0
+  		while helper < array.length
+  			if a == array[helper]
+  				counter += 1
+  			end
+  			helper += 1
+  		end
+  		new_hash[a] = counter
+  	end
+  	new_hash
 end
  
