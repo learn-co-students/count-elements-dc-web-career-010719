77cdfa05d98ef9e0eb905bf403951321720de00a
dcjones1 <dcejjones@gmail.com>
2019-01-08 17:41:19 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
-  # code goes here
+  count_hash = {}
+  array.each do |animal|
+    if count_hash.has_key?(animal)
+      count_hash[animal] += 1
+    else
+      count_hash[animal] = 1
+    end
+  end
+  count_hash
 end
- 
